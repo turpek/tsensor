28715260fadfb45ac3713ff8c880219d525f1555
Guilherme T. Faria <gui.faria.96@hotmail.com>
2026-04-10 18:08:28 -0300

    feat(app): integrar aquisição serial via thread secundária no Flask
    - Configura o ponto de entrada da aplicação Flask (app.py) para iniciar a coleta serial.
    - Adiciona a função serial_acquisition para gerenciar o handler LM35 e a leitura da porta.
    - Implementa o uso de threading.Thread para execução assíncrona da coleta.
    - Garante que a aquisição inicie apenas uma vez no modo debug usando WERKZEUG_RUN_MAIN.
    - Define novos streams de dados (buffer_stream, history_stream) em extensions.py.

--- src/tsensor/app.py
+++ src/tsensor/app.py
@@ -1,13 +1,40 @@
+import threading
 from flask import Flask
+from loguru import logger
 from tsensor.routes.home import home_route
 from tsensor.routes.api import api_route
-
+from tsensor.extensions import data_stream, buffer_stream
+from tsensor.core.handlers import LM35Handler
+from tsensor.core.serial_reader import serial_reading
 
 app = Flask(__name__)
-app.secret_key = 'SUA_CHAVE_SECRETA'
+app.secret_key = "SUA_CHAVE_SECRETA"
 
 app.register_blueprint(home_route)
 app.register_blueprint(api_route)
 
-if __name__ == '__main__':
+
+def serial_acquisition():
+    handler = LM35Handler(data_stream, buffer_stream, 1_000_000, 1800)
+
+    logger.info("Iniciando leitura serial simulada...")
+    serial_reading(
+        port="/dev/ttyACM1",
+        baudrate=115200,
+        samples=1_000_000,
+        handler=handler,
+        timeout=1,
+    )
+
+
+if __name__ == "__main__":
+    import os
+
+    # Garante que a thread de aquisição inicie apenas uma vez no modo debug do Flask
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        acquisition_thread = threading.Thread(
+            target=serial_acquisition, daemon=True
+        )
+        acquisition_thread.start()
+
     app.run(debug=True)
--- src/tsensor/extensions.py
+++ src/tsensor/extensions.py
@@ -2,4 +2,7 @@ from tsensor.core.data_stream import DataStream
 
 
 TOTAL_SAMPLES = 1_000_000
+TOTA_TEMPORAL_SAMPLES = 1_000
 data_stream = DataStream(total_samples=TOTAL_SAMPLES)
+buffer_stream = DataStream(total_samples=TOTA_TEMPORAL_SAMPLES)
+history_stream = DataStream(total_samples=TOTA_TEMPORAL_SAMPLES)
